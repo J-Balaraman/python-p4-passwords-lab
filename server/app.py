64b3f07aa19ca169db364875fb376468defc039e
James Balaraman <james.balaraman@gmail.com>
2024-04-20 16:35:47 -0600

    I think its starting to come together

--- server/app.py
+++ server/app.py
@@ -27,17 +27,32 @@ class Signup(Resource):
         db.session.commit()
         return user.to_dict(), 201
 
-class CheckSession(Resource):
-    pass
-
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        user = User.query.filter(User.username == json['username']).first()
+        session['username'] = json['username']
+        session['user_id'] = user.id
+        return user.to_dict()
 
 class Logout(Resource):
-    pass
-
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+    
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        print(user)
+        if user:
+            return user.to_dict(), 200
+        return {}, 204
+    
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
